0c81eef2319ef0ea4be8c044142aa94916d35734
towm1204 <17towm@gmail.com>
2020-04-05 23:39:38 -0700

    initialized app

---
+++ app/src/main/java/com/towm1204/dotify/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.towm1204.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
